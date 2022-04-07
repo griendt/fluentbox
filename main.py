5f8a4b0acc8d02f2c0343f807bcf937a95f18e31
alex <alex@e-cube.nl>
2022-04-07 19:37:42 +0200

    Implement functions for list, dict and set-like container classes

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
-import collections.abc
-from typing import Callable, Generator, Sequence
+import collections.abc as abc
 
 
-class Collection:
-    _items: collections.abc.Collection
+class Silver:
+    _items: abc.Collection
 
-    def __init__(self, items: collections.abc.Collection):
-        self._items = items
+    def __init__(self, items):
+        if isinstance(items, abc.Collection):
+            self._items = items
+
+        else:
+            self._items = [items]
 
     def __contains__(self, obj: object) -> bool:
         return obj in self._items
@@ -17,30 +20,29 @@ class Collection:
         return len(self._items)
 
     def __iter__(self):
-        if isinstance(self._items, dict):
-            for key, value in self._items.items():
-                yield key, value
-
         for value in self._items:
             yield value
 
-    def all(self):
-        if isinstance(self._items, Generator):
-            return self.do().all()
-
+    def all(self) -> abc.Collection:
         return self._items
 
-    def do(self) -> Collection:
-        # Exhaust any underlying Generator by casting it to its appropriate item type
-        return type(self)(type(self._items)(self._items))
+    def each(self, callback: abc.Callable) -> Silver:
+        for item in self:
+            callback(item)
 
-    def each(self, callback: Callable):
-        def generator(obj):
-            for item in obj:
-                callback(item)
-                yield item
+        return self
 
-        return type(self)(generator(self))
+    def items(self):
+        if isinstance(self._items, abc.Mapping):
+            for key, value in self._items.items():
+                yield key, value
+
+        elif isinstance(self._items, abc.Iterable):
+            for key, value in enumerate(self._items):
+                yield key, value
+
+        else:
+            raise TypeError(f"Item type {type(self._items)} should implement Iterable")
 
     def first(self, or_fail: bool = False):
         for item in self:
@@ -52,68 +54,58 @@ class Collection:
     def first_or_fail(self):
         return self.first(or_fail=True)
 
-    def items(self):
-        if isinstance(self._items, dict):
-            for key, value in self._items.items():
-                yield key, value
+    def map(self, callback: abc.Callable) -> Silver:
+        new_items = type(self._items)()
 
-        elif isinstance(self._items, Sequence):
-            for key, value in enumerate(self._items):
-                yield key, value
+        if isinstance(new_items, abc.MutableMapping):
+            for key, value in self.items():
+                new_items[key] = callback(value)
+
+        elif isinstance(new_items, abc.MutableSequence):
+            for _, value in self.items():
+                new_items.append(callback(value))
+
+        elif isinstance(new_items, abc.MutableSet):
+            for _, value in self.items():
+                new_items.add(callback(value))
 
         else:
-            raise ValueError(f"Item type {self._item_type} is not a dict nor implements Sequence")
+            raise TypeError(f"Item type {type(self._items)} should be Mutable")
 
-    def map(self, callback: Callable) -> Collection:
-        def generator(obj):
-            for item in obj:
-                yield callback(item)
+        return type(self)(new_items)
 
-        return type(self)(generator(self))
+    def filter(self, callback: abc.Callable = None):
+        new_items = type(self._items)()
 
-    def filter(self, callback: Callable = None):
-        def generator(obj):
-            for item in obj:
-                if callback is None:
-                    if item:
-                        yield item
-                elif callback(item):
-                    yield item
+        if callback is None:
+            callback = bool
 
-        return type(self)(generator(self))
+        if isinstance(new_items, abc.MutableMapping):
+            for key, value in self.items():
+                if callback(value):
+                    new_items[key] = value
 
-    def to(self, cls: type):
-        return cls(self._items)
+        elif isinstance(new_items, abc.MutableSequence):
+            for _, value in self.items():
+                if callback(value):
+                    new_items.append(value)
 
+        elif isinstance(new_items, abc.MutableSet):
+            for _, value in self.items():
+                if callback(value):
+                    new_items.add(value)
 
-def collection_of(cls: type) -> type:
-    if not issubclass(cls, collections.abc.Collection):
-        raise TypeError(f"Cannot make concrete Collection class of type {cls}")
+        else:
+            raise TypeError(f"Item type {type(self._items)} should be Mutable")
 
-    collection_cls = Collection
-    collection_cls.__name__ = cls.__name__ + "Collection"
-    collection_cls.__annotations__["_items"] = cls
-    collection_cls._item_type = cls
-
-    return collection_cls
-
-
-def collect(*args):
-    if len(args) > 1:
-        return Collection(args)
-
-    if isinstance(args[0], list):
-        return Collection(args[0])
-
-    return Collection(list(args))
+        return type(self)(new_items)
 
 
 if __name__ == '__main__':
-    x = (Collection({3, 4, 5})
+    x = (Silver({3, 4, 5})
          .map(lambda i: ((i := i + 1), (i + 2))[-1])
          .filter(lambda item: item < 30)
          .each(lambda i: print(f"I am {i}!"))
-         .do()
          )
 
-    print(x._items)
+    print(x.all())
