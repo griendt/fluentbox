39f45bdaf65fac2819e477c3e454c172bee5e6c8
alex <avdg.lucerna@gmail.com>
2022-05-29 20:59:18 +0200

    Actual 0.1.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vessel",
-    version="0.1.2",
+    version="0.1.0",
     description="Fluent chainable interface for container data types",
     url="https://github.com/griendt/vessel",
     author="Alex van de Griendt",
